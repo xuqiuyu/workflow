77a0f968dfe53266d496cc3c6b347f66222bb04b
breeze.xu <breeze.xu@Breeze.localdomain>
2019-01-16 11:20:45 +0800

    add description doc

--- db/wf_node_action.sql
+++ db/wf_node_action.sql
@@ -9,15 +9,15 @@ create table wf_node_action (
   primary key (id)
 );
 
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'BD_CS_INIT', 1, 2);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'BD_CS_APPROVE', 1, 3);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_LEADER_ASSIGN', 2, 3);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_MAKER_APPROVE', 3, 4);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_MAKER_REJECT', 3, 1);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_SERNIOR_APPROVE', 3, null);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_CKECKER_APPROVE', 4, 5);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'KYC_CKECKER_REJECT', 4, 1);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'COMPLIANCE_APPROVE', 5, null);
-INSERT INTO  wf_node_action(wf_code, name, current_node_id, next_node_id) VALUES('CLIENT_ONBOARDING', 'COMPLIANCE_REJECT', 5, 3);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'BD_CS_INIT', 1, 2);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'BD_CS_APPROVE', 1, 3);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_LEADER_ASSIGN', 2, 3);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_MAKER_APPROVE', 3, 4);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_MAKER_REJECT', 3, 1);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_SERNIOR_APPROVE', 3, null);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_CKECKER_APPROVE', 4, 5);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'KYC_CKECKER_REJECT', 4, 1);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'COMPLIANCE_APPROVE', 5, null);
+INSERT INTO  wf_node_action(wf_code, name, current_node, next_node) VALUES('CLIENT_ONBOARDING', 'COMPLIANCE_REJECT', 5, 3);
 
 
